5bb8b555691de8eaa4c401a2abc6d577a41653c5
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2021-07-06 14:58:50 +0300

    add gender reference to users table

---
+++ database/migrations/2021_07_06_114852_add_gender_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddGenderToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('partner_email')->nullable();
+
+            $table->unsignedBigInteger('gender_id')->index()->nullable();
+            $table->foreign('gender_id')->references('id')->on('genders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['gender_id']);
+            $table->dropIndex(['gender_id']);
+            $table->dropColumn(['gender_id', 'partner_email']);
+        });
+    }
+}
