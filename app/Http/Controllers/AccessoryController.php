54f6deb58382a14de26e47e9e74927e433277762
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2021-07-12 17:33:45 +0300

    fix validation

--- app/Http/Controllers/AccessoryController.php
+++ app/Http/Controllers/AccessoryController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\Accessory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 
 class AccessoryController extends Controller
 {
@@ -25,12 +26,14 @@ class AccessoryController extends Controller
         // $user = User::with('locations')->findOrFail($id);
 
         $validatedData = $request->validate([
-            'accessories.*' => 'required|unique:accessories,name',
-            'preferences.*' => 'required',
+            // 'accessories.*' => ['required', Rule::unique('accessories', 'name')->ignore($id, 'user_id')],
+            // 'accessories.*' => 'required_with:preferences[]',
+            // 'preferences.*' => 'required_with:accessories[]',
         ]);
 
-        foreach ($validatedData['accessories'] as $key => $accessory) {
-            $new_accessory = Accessory::firstOrCreate(['name' => $accessory, 'preference_id' => $validatedData['preferences'][$key], 'user_id' => $id]);
+        foreach ($request->accessories as $key => $accessory) {
+            // $new_accessory = Accessory::firstOrCreate(['name' => $accessory, 'preference_id' => $validatedData['preferences'][$key], 'user_id' => $id]); // 1 accessory to MANY preferences
+            $new_accessory = Accessory::firstOrCreate(['name' => $accessory, 'user_id' => $id], ['preference_id' => $request->preferences[$key]]);  // 1 accessory to 1 preference
         }
         return redirect()->route('quest');
     }
