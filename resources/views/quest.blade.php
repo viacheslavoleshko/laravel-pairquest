6827be262c791f37ffa8fa01f6cfe6c44e5d2f7d
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2021-07-15 16:14:47 +0300

    fix quest style

--- resources/views/quest.blade.php
+++ resources/views/quest.blade.php
@@ -15,25 +15,23 @@
             <form method="POST" action="{{ route('quest-duration', ['user' => Auth::user()->id]) }}" >
                 @csrf
                 @foreach ($durations as $duration)
-                <button type="submit" name="duration" class="btn-link" value="{{ $duration->id }}">{{ $duration->name }}</button>
+                <button type="submit" name="duration" class="btn-quest" value="{{ $duration->id }}">{{ $duration->name }}</button>
                 @endforeach
 
-                <select class="form-control form-input-items" name="user_level">
-                    <option selected disabled>Select collective user level</option>
-                    @foreach ($user_levels as $key => $user_level)
-                        <option value="{{ $user_level }}">{{ $key }}</option>
-                    @endforeach
-                </select>
-                
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+                <div class="form-outline mb-4">
+                    <select class="form-select @error('user_level') is-invalid @enderror" name="user_level" required autocomplete="user_level">
+                        <option selected disabled>Select collective user level</option>
+                        @foreach ($user_levels as $key => $user_level)
+                            <option value="{{ $user_level }}">{{ $key }}</option>
+                        @endforeach
+                    </select>
+
+                    @error('user_level')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
 
                 <a class="reg-link" href="{{ route('prefs') }}">Редагувати вподобання</a>
             </form>
